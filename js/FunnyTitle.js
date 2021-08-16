9833750563b6c3dc122230773edf7eb5ed0b51f3
KHLFYY <KHLFYY@outlook.com>
2021-08-16 22:06:44 +0800

    Site updated: 2021-08-16 22:06:44

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,13 +3,13 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         $('[rel="icon"]').attr('href', "/img/favicon.png");
          document.title = '网站崩溃了Σ(っ°Д°;)っ';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = '啪的一下好了ヽ(･ω･´ﾒ)' + OriginTitle;
+         $('[rel="icon"]').attr('href', "/img/favicon.png");
+         document.title = '啪的一下就好了ヽ(･ω･´)--'+ OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
