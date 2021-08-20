445e2cc08b4e331d9ba5a57829e1bbfde7c3374c
KHLFYY <KHLFYY@outlook.com>
2021-08-20 13:09:59 +0800

    Site updated: 2021-08-20 13:09:58

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/favicon.png");
-         document.title = '网站崩溃了Σ(っ°Д°;)っ';
+         document.title = '怎么不看我了Σ(っ°Д°;)っ';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/img/favicon.png");
-         document.title = '啪的一下就好了ヽ(･ω･´)--'+ OriginTitle;
+         document.title = '欢迎回来ヽ(･ω･´)--'+ OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
